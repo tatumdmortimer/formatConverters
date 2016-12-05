b36d34e9ddb54c6f11ea44fd76d7fa08569844a2
Tatum Mortimer <tatumdmortimer@gmail.com>
2016-12-05 15:29:57 -0600

    Now checks for gene orientation and reverse complements genes on the - straind

--- alignmentsFromBed.py
+++ alignmentsFromBed.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python
 
-#This script will take a bed file of intervals and an alignment and produce
-#a separate fasta alignment roduce nexus alignments of fragments > 1kb 
+# This script will take a bed file of intervals and an alignment and produce
+# a separate fasta alignment roduce nexus alignments of fragments > 1kb
 
 import sys
 import argparse
 from Bio import AlignIO
 
+
 def get_args():
     parser = argparse.ArgumentParser(description='Produces alignments from bed \
 file intervals and fasta alignment')
     parser.add_argument("align", help="Fasta alignment",
-    type=argparse.FileType('r'))
+                        type=argparse.FileType('r'))
     parser.add_argument("bed", help="BED file with intervals")
     return parser.parse_args()
+
 
 def read_bed_file(bed):
     intervalDict = {}
@@ -21,18 +23,23 @@ def read_bed_file(bed):
     for index, line in enumerate(bedfile):
         if index > 0:
             line = line.strip().split()
-            start = int(line[1]) - 1
-            stop = int(line[2]) - 1
+            start = int(line[1])
+            stop = int(line[2])
             name = line[3]
-            intervalDict[name] = (start,stop)
+            orientation = line[5]
+            intervalDict[name] = (start, stop, orientation)
     bedfile.close()
     return intervalDict
+
 
 def make_alignments(intervalDict, align):
     alignment = AlignIO.read(align, "fasta")
     for interval, startStop in intervalDict.iteritems():
-        AlignIO.write(alignment[:,startStop[0]:startStop[1]], 
-        interval + ".fasta", 'fasta')
+        newAlign = alignment[:, startStop[0]:startStop[1]]
+        if startStop[2] == "-":
+            for rec in newAlign:
+                rec.seq = rec.seq.reverse_complement()
+        AlignIO.write(newAlign, interval + ".fasta", 'fasta')
 
 args = get_args()
 intervalDict = read_bed_file(args.bed)
